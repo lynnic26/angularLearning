9f8216b75fe779a78c0686bdbf6bf55867df50e0
GuanNing <guanning@patsnap.com>
2017-05-13 20:50:42 +0800

    add a controller

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-var app = angular.module('store', []);
+(function () {
+    var gem = {
+        name: 'Azurite',
+        price: 300
+    }
+    var app = angular.module('store', []);
+    app.controller('StoreController', function () {
+        this.product = gem;
+    });
+
+})();
